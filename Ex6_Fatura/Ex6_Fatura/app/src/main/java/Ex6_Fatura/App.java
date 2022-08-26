275d46f4ce33c571a1d7239875cde80be096bbd1
kennedy <dkennyvs@gmail.com>
2022-08-26 10:30:37 -0300

    Valor da fatura

--- Ex6_Fatura/Ex6_Fatura/app/src/main/java/Ex6_Fatura/App.java
+++ Ex6_Fatura/Ex6_Fatura/app/src/main/java/Ex6_Fatura/App.java
@@ -24,7 +24,7 @@ public class App {
 
     public static void main(String[] args) {
       
-        Invoice inv = new Invoice(1, "Mousepad", -10, 50.0f);
+        Invoice inv = new Invoice(1, "Mousepad", 10, 50.0f);
         
         System.out.println("Valor total: R$ " + inv.getInvoiceAmount());
     }
